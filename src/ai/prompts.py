1188951978b5ed989ce03e7bdf6df60c8eaa8851
Fransua374 <fransuaraaths8@gmail.com>
2026-07-16 15:45:06 +0200

    Create prompts.py

---
+++ src/ai/prompts.py
@@ -0,0 +1,17 @@
+EMAIL_PROMPT = """
+Write a professional business email from the following information:
+
+{input}
+"""
+
+SUMMARY_PROMPT = """
+Summarise the following text clearly:
+
+{text}
+"""
+
+FAQ_PROMPT = """
+Answer the following customer question professionally:
+
+{question}
+"""
